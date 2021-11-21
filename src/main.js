d37f533899aee22a29cf38a4414b07fdc1a6ef56
Rizky Ikhwan <rizkyikhwan.ri@gmail.com>
2021-11-21 16:19:40 +0700

    First Commit
    add components navbar

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "@popperjs/core"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./assets/css/main.css"
+import "./script/components/navbar.js"
+import main from "./script/app.js"
+
+document.addEventListener("DOMContentLoaded", main)
---
+++ src/script/components/navbar.js
@@ -0,0 +1,23 @@
+class NavBar extends HTMLElement {
+  connectedCallback() {
+    this.render()
+  }
+
+  render() {
+    this.innerHTML = `
+      <h4 id="navbar-logo">MAKE FOOD</h4>
+      <div class="menu-toggle" id="mobile-menu">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </div>
+      <ul class="nav-menu">
+        <li><a href="#" class="nav-links">Home</a></li>
+        <li><a href="#" class="nav-links">Recipe</a></li>
+        <li><a href="#" class="nav-links">About</a></li>
+      </ul>
+    `
+  }
+}
+
+customElements.define('nav-bar', NavBar)
